b593d1c0c472beb24b7be4028cdcb310e18a8206
Skorzec francois <fskorzec@outlook.com>
2018-10-19 00:03:23 -0400

    Added view doc

--- src/pages/View.tsx
+++ src/pages/View.tsx
@@ -10,7 +10,25 @@ export function View() {
         View
       </Typography>
       <Typography variant="subtitle1" gutterBottom>
-     
+      To be able to interact with the Stores, it is possible to use an instance of the Subscriber class, which includes the dispatcher so as not to expose it directly.
+      <br />
+      This class will only expose the methods of subscribing and sending Actions.
+      <Prism language="typescript" text={`
+      // FLux Def file
+      const dispatcher  = new Dispatcher()           ;
+      const pubSub      = new Subscriber(dispatcher) ;
+      ...
+
+      // View file
+      const subscription = wrapper.subscribe<any, any>(simpleStore.id, "double", (state) => {
+        return state;
+      }, (state) => {
+        done();
+        subscription.off();
+      });
+  
+      wrapper.sendAction({ type: "double" });
+      `} />
       </Typography>
     </React.Fragment>
   );
