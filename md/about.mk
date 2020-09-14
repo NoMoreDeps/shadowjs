ef92c4d5b14dd881815cdaf8c2c1f05f93a509d4
NoMorDeps <fskorzec@outlook.com>
2020-09-14 12:25:27 -0400

    Updated about section

--- md/about.mk
+++ md/about.mk
@@ -3,10 +3,5 @@
   Passionate about open source, the idea of S-Flux came after discovered React & Flux Pattern.
   I was not conviced by using Redux, so I decided to implement S-Flux.
 
-  I always wished I could do more, to be able to use it to create websites, or stylized documentation, blogs etc...
-  The lack of stylization possibilities often frustrated me, and the difficulty to extend the language in a simple way too. 
-
-  I decided to make my contribution in order to offer new possibilities in this field.
-
   See the project on [Github](https://github.com/NoMoreDeps/shadow-flux "new")
 }}
