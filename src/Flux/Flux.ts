7feecd2f444bdb3702139c6e2aadb884580bee83
NoMorDeps <fskorzec@outlook.com>
2020-09-14 08:43:21 -0400

    fix

--- src/Flux/Flux.ts
+++ src/Flux/Flux.ts
@@ -1,7 +1,12 @@
+import { TGlobalConfig } from "./AppStore";
+
 export default {voidExp: 0};
+declare var global_config: TGlobalConfig;
 
 if (window.location.pathname.length > 1) {
-  const path = window.location.pathname.endsWith("/") ?  window.location.pathname.substr(0, window.location.pathname.length -1): window.location.pathname ;
+  const path = (window.location.pathname.endsWith("/") ?  window.location.pathname.substr(0, window.location.pathname.length -1): window.location.pathname)
+    ?.replace(global_config.root, "");
+
   window.history.pushState({ path }, "", path);
   console.log({ path }, "", path)
 }
