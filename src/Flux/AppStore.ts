217d64e71ae6991b37bb53eaea616aa2ca3e1f9c
NoMorDeps <fskorzec@outlook.com>
2020-09-13 23:21:02 -0400

    re fix

--- src/Flux/AppStore.ts
+++ src/Flux/AppStore.ts
@@ -79,7 +79,7 @@ const actions = {
       }
       
       if (!item.md) {
-        const md = await (await fetch(`/shadowjs/md${item?.path !== "/" ? item?.path : "/shadowjs/index"}.mk`)).text();
+        const md = await (await fetch(`/shadowjs/md${item?.path !== "/" ? item?.path : "/shadowjs/md/index"}.mk`)).text();
         item.md  = md;
       }
       
